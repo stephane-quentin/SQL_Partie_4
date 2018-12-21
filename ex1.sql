596cbee1967f7b18ca92a71d1beab8fe6ffbd4ef
Stiouf <stephane.quentin@live.fr>
2018-12-21 15:06:12 +0100

    Partie_4

---
+++ ex1.sql
@@ -0,0 +1,13 @@
+USE `webDevelopment`;
+INSERT INTO `languages` (`language`, `version`)
+-- INSERT INTO pour insérer des données dans la table languages,
+-- et on indique les colonnes que l'on veut insérer language et version
+VALUES ('JavaScript', '5'),
+       ('PHP', '5.2'),
+       ('PHP', '5.4'),
+       ('HTML', '5.1'),
+       ('JavaScript', '6'),
+       ('JavaScript', '7'),
+       ('JavaScript', '8'),
+       ('PHP', '7');
+-- Et on indique les valeurs que l'on veut mettre dans les colonnes dans le meme ordre
---
+++ ex2.sql
@@ -0,0 +1,6 @@
+USE `webDevelopment`;
+INSERT INTO `frameworks` (`framework`, `version`)
+VALUES ('Symfony', '2.8'),
+       ('Symfony', '3'),
+       ('Jquery', '1.6'),
+       ('Jquery', '2.10');
